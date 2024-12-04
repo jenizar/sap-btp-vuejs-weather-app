158c5549f25fe52cf61dfc7f145bf741bce1c097
John Eswin Nizar <jenizar@users.noreply.github.com>
2024-12-04 19:01:02 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
